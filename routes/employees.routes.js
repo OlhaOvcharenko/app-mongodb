d726019ba0ae8c1d56186ae992e1bcb1677424f1
Olha <ovcharenko.olga.2014@gmail.com>
2023-12-16 14:26:26 +0300

    add method populate to employees routes

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.module')
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -15,18 +15,17 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const employee = await Employee.findOne().skip(rand);
-    if(!employee) res.status(404).json({ message: 'Not found' });
+    const employee = await Employee.findOne().skip(rand).populate('department');
+    if (!employee) res.status(404).json({ message: 'Not found' });
     else res.json(employee);
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).json({ message: err });
   }
 });
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const employee = await Employee.findById(req.params.id);
+    const employee = await Employee.findById(req.params.id).populate('department');
     if(!employee) res.status(404).json({ message: 'Not found' });
     else res.json(employee);
   }
@@ -38,8 +37,11 @@ router.get('/employees/:id', async (req, res) => {
 router.post('/employees', async (req, res) => {
   try {
     const { firstName, lastName, department, salary } = req.body;
+
     const newEmployee = new Employee({ firstName, lastName, department, salary });
     await newEmployee.save();
+
+
     res.json({ message: 'OK' });
   }
   catch(err) {
